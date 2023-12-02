c33952ee0010f8b938a6e4ee5b7ec3a5dc567530
hirokirigaya <privz286@gmail.com>
2023-12-01 22:07:57 -0300

    feat(nio): ZipOutputStream

---
+++ src/co/danieldev/oneforall/javacore/Qnio/test/ZipOutputStreamTest01.java
@@ -0,0 +1,33 @@
+package co.danieldev.oneforall.javacore.Qnio.test;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipOutputStreamTest01 {
+    public static void main(String[] args) {
+        Path zipFile = Paths.get("folder/file.zip");
+        Path sourceFiles = Paths.get("folder/subfolder1/subsubfolder1");
+        zip(zipFile, sourceFiles);
+
+    }
+
+    private static void zip(Path zipFile, Path pathSource) {
+        try (ZipOutputStream zipStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+             DirectoryStream<Path> directoryStream = Files.newDirectoryStream(pathSource);
+        ) {
+            for (Path file: directoryStream) {
+                ZipEntry zipEntry = new ZipEntry(file.getFileName().toString());
+                zipStream.putNextEntry(zipEntry);
+                Files.copy(file, zipStream);
+                zipStream.closeEntry();
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
